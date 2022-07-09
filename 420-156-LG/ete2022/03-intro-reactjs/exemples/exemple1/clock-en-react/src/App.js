d059c4646494fb25e1a0e8c3eb08d4730c8e3a29
Gabriel Gamy <gabrielyakpaoro@gmail.com>
2022-07-09 14:52:19 -0700

    420-156-LG - e22 - semaine3 - Intro React

---
+++ 420-156-LG/ete2022/03-intro-reactjs/exemples/exemple1/clock-en-react/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: null,
+      showTime: true
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setTime();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  setTime = () => {
+    var now = new Date();
+    const time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    this.setState({ time });
+  }
+
+  toggleTimeHandler = () => {
+    const showTime = !this.state.showTime;
+    this.setState({ showTime });
+  }
+
+  render() {
+    return (
+      <>
+        <header>Clock App</header>
+        <div className="clock-container">
+          { this.state.showTime && <div id="clock">{this.state.time}</div> }
+          <button className="toggle-btn" onClick={this.toggleTimeHandler}>
+            { this.state.showTime ? "Hide clock" : "Show clock" }
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
