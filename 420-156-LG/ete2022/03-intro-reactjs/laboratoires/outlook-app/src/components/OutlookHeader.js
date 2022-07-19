e3d8af6d6cc11be9767f566ae413798774829fd7
Gabriel Gamy <gabrielyakpaoro@gmail.com>
2022-07-19 08:13:41 -0700

    420-156-LG - Intro React - Laboratoire

---
+++ 420-156-LG/ete2022/03-intro-reactjs/laboratoires/outlook-app/src/components/OutlookHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class OutlookHeader extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 className="header">Outlook - Collège Lionel-Groulx</h1>
+        <h2 className="header">Envoyer le courriel</h2>
+      </>
+    );
+  }
+}
+
+export default OutlookHeader;
