82f5b7f7300d95862b059a2c2f9f36c9bb7e0626
Gabriel Gamy <gabrielyakpaoro@gmail.com>
2022-09-15 23:31:53 -0700

    420-156-LG - Deploy to Google Play - DiscoverCanada

--- 420-156-LG/ete2022/11-localization/discovercanada/data/languages.js
+++ 420-156-LG/ete2022/11-localization/discovercanada/data/languages.js
@@ -1,43 +1,43 @@
 import { I18n } from "i18n-js";
-import * as Localization from 'expo-localization';
+import * as Localization from "expo-localization";
 
 const fr = {
-  app_title: 'Découvrir le Canada',
-  states: 'Provinves',
-  'Alberta': 'Alberta',
-  'Colombie-Britannique': 'Colombie-Britannique',
-  'Île-du-Prince-Édouard': 'Île-du-Prince-Édouard',
-  'Manitoba': 'Manitoba',
-  'Nouveau-Brunswick': 'Nouveau-Brunswick',
-  'Nouvelle-Écosse': 'Nouvelle-Écosse',
-  'Ontario': 'Ontario',
-  'Québec': 'Québec',
-  'Saskatchewan': 'Saskatchewan',
-  'Terre-Neuve-et-Labrador': 'Terre-Neuve-et-Labrador',
-  'Territoires du Nord-Ouest': 'Territoires du Nord-Ouest',
-  'Nunavut': 'Nunavut',
-  'Yukon': 'Yukon',
-  learn_more: 'Découvrir'
+  app_title: "Découvrir le Canada",
+  states: "Provinves",
+  Alberta: "Alberta",
+  "Colombie-Britannique": "Colombie-Britannique",
+  "Île-du-Prince-Édouard": "Île-du-Prince-Édouard",
+  Manitoba: "Manitoba",
+  "Nouveau-Brunswick": "Nouveau-Brunswick",
+  "Nouvelle-Écosse": "Nouvelle-Écosse",
+  Ontario: "Ontario",
+  Québec: "Québec",
+  Saskatchewan: "Saskatchewan",
+  "Terre-Neuve-et-Labrador": "Terre-Neuve-et-Labrador",
+  "Territoires du Nord-Ouest": "Territoires du Nord-Ouest",
+  Nunavut: "Nunavut",
+  Yukon: "Yukon",
+  learn_more: "Découvrir",
 };
 
 const en = {
-  app_title: 'Discover Canada',
-  states: 'States',
-  'Alberta': 'Alberta',
-  'Colombie-Britannique': 'British Columbia',
-  'Île-du-Prince-Édouard': 'Prince Edouard Island',
-  'Manitoba': 'Manitoba',
-  'Nouveau-Brunswick': 'New-Brunswick',
-  'Nouvelle-Écosse': 'Nova-Scotia',
-  'Ontario': 'Ontario',
-  'Québec': 'Quebec',
-  'Saskatchewan': 'Saskatchewan',
-  'Terre-Neuve-et-Labrador': ' Newfoundland and Labrador',
-  'Territoires du Nord-Ouest': 'Northwest Territories',
-  'Nunavut': 'Nunavut',
-  'Yukon': 'Yukon',
-  learn_more: 'Lean More'
-}
+  app_title: "Discover Canada",
+  states: "States",
+  Alberta: "Alberta",
+  "Colombie-Britannique": "British Columbia",
+  "Île-du-Prince-Édouard": "Prince Edouard Island",
+  Manitoba: "Manitoba",
+  "Nouveau-Brunswick": "New-Brunswick",
+  "Nouvelle-Écosse": "Nova-Scotia",
+  Ontario: "Ontario",
+  Québec: "Quebec",
+  Saskatchewan: "Saskatchewan",
+  "Terre-Neuve-et-Labrador": " Newfoundland and Labrador",
+  "Territoires du Nord-Ouest": "Northwest Territories",
+  Nunavut: "Nunavut",
+  Yukon: "Yukon",
+  learn_more: "Learn More",
+};
 
 const i18n = new I18n({ en, fr });
 
